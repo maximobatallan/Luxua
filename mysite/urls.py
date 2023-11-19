1307d9e10baa674442da5f2caa240d33957aad40
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-11-19 02:16:45 -0300

    Add files via upload

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,8 @@
-"""mysite URL Configuration
+"""
+URL configuration for Mecha project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +16,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from tasks import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('admin/', admin.site.urls, name='admin'),
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
+    path('agregar/<int:producto_id>/', views.agregar_producto, name="Add"),
+    path('eliminar/<int:producto_id>/', views.eliminar_producto, name="Del"),
+    path('restar/<int:producto_id>/', views.restar_producto, name="Sub"),
+    path('limpiaritems/<int:producto_id>/', views.limpiar_carrito_item, name="CLSK"),
+    path('limpiar/', views.limpiar_carrito, name="CLS"),
+    path('gallery/', views.galeriaprueba, name='gallery'),
+    path('productdetails/<int:producto_id>/', views.detalleproducto, name='PD'),
+    path('cargaproducto/', views.producto, name='cargaproducto'),
+    path('datos/', views.datos, name='datos'),
+    path('catproducto/<str:catproducto>/', views.catproducto, name='catproducto'),
+    path('categoria/', views.categoria, name='categoria'),
+    ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
