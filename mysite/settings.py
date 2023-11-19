a3c4f8b2a4db8ae263af09356b2b52808708f6c1
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-11-19 15:51:56 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tasks'
+    'tasks',
+    'storages',
 ]
 
 MIDDLEWARE = [
